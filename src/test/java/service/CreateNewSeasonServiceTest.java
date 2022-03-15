b2931d1afede3833b94890555f6d9202d3f37ac6
19jest01 <19jest01@13631-STUD-D.HIG-AD.hig.se>
2022-03-15 11:16:58 +0100

    New tests

---
+++ src/test/java/service/CreateNewSeasonServiceTest.java
@@ -0,0 +1,59 @@
+package service;
+
+import broker.BrokerFactory;
+import broker.SeasonBroker;
+import db.DbConn;
+import domain.League;
+import domain.Season;
+import exceptions.SportstatServiceException;
+import java.sql.Date;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * A service that creates a new season
+ * @author jenniferstreit
+ */
+public class CreateNewSeasonServiceTest {
+    private final String mars = "2022-03-22";
+    private final String april = "2022-04-22";
+
+    @Test
+    public void testCreatingNewSeason() {
+        CreateNewSeasonService testService = new CreateNewSeasonService(null, mars, april);
+        DbConn dbConn = mock(DbConn.class);
+        BrokerFactory brokerFactory = getMockedBrokerFactoryWithBrokersSetup();
+        testService.init(dbConn, brokerFactory);
+        Season s = brokerFactory.getSeasonBroker().create();
+        assertEquals(Date.valueOf(mars), s.getStartDate());
+    }
+    
+    @Test
+    public void testCreatingNewSeasonException() {
+        CreateNewSeasonService testService = new CreateNewSeasonService(null, april, mars);
+        DbConn dbConn = mock(DbConn.class);
+        BrokerFactory brokerFactory = getMockedBrokerFactoryWithBrokersSetup();
+        testService.init(dbConn, brokerFactory);
+        Season s = brokerFactory.getSeasonBroker().create();
+        assertThrows(SportstatServiceException.class, () -> testService.execute());
+    }
+    
+    private BrokerFactory getMockedBrokerFactory() {
+        SeasonBroker seasonBroker = mock(SeasonBroker.class);
+        BrokerFactory brokerFactory = mock(BrokerFactory.class);
+        when(brokerFactory.getSeasonBroker()).thenReturn(seasonBroker);
+        return brokerFactory;
+    }
+    
+    private BrokerFactory getMockedBrokerFactoryWithBrokersSetup() {
+        BrokerFactory brokerFactory = getMockedBrokerFactory();
+        Season season = mock(Season.class);
+        SeasonBroker seasonBroker = brokerFactory.getSeasonBroker();
+        when(seasonBroker.create()).thenReturn(season);
+        when(season.getStartDate()).thenReturn(Date.valueOf(mars));
+        return brokerFactory;
+    }
+    
+}
---
+++ src/test/java/service/GetLeaguesBySportsNameServiceTest.java
@@ -0,0 +1,52 @@
+package service;
+
+import broker.BrokerFactory;
+import broker.LeagueBroker;
+import broker.SportBroker;
+import db.DbConn;
+import domain.League;
+import domain.Sport;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+/**
+ *
+ * @author jenniferstreit
+ */
+public class GetLeaguesBySportsNameServiceTest {
+    private final String name = "fotboll";
+    
+    @Test
+    public void testGetAllLeauges() {
+        GetLeaguesBySportsNameService testService = new GetLeaguesBySportsNameService(name);
+        DbConn dbConn = mock(DbConn.class);
+        BrokerFactory brokerFactory = getMockedBrokerFactoryWithBrokersSetup();
+        testService.init(dbConn, brokerFactory);
+        List<League> leagues = testService.execute();
+        assertEquals("list", leagues.toString());
+    }
+    
+    private BrokerFactory getMockedBrokerFactory() {
+        SportBroker sportBroker = mock(SportBroker.class);
+        BrokerFactory brokerFactory = mock(BrokerFactory.class);
+        when(brokerFactory.getSportBroker()).thenReturn(sportBroker);
+        return brokerFactory;
+    }
+    
+    private BrokerFactory getMockedBrokerFactoryWithBrokersSetup() {
+        BrokerFactory brokerFactory = getMockedBrokerFactory();
+        Sport sport = mock(Sport.class);
+        List<League> list = mock(List.class);
+        SportBroker sportBroker = brokerFactory.getSportBroker();
+        when(sportBroker.findByName("fotboll")).thenReturn(sport);
+        when(sport.getAllConnectedLeagues()).thenReturn(list);
+        when(list.toString()).thenReturn("list"); 
+        
+        return brokerFactory;
+    }
+    
+}
